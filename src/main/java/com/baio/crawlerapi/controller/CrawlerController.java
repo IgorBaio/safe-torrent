c00b3d8b2164319c2ec202485d70909f26760bac
igorbaio <igorbaiosoares@gmail.com>
2024-06-21 08:21:32 -0300

    fix conflicts

--- src/main/java/com/baio/crawlerapi/controller/CrawlerController.java
+++ src/main/java/com/baio/crawlerapi/controller/CrawlerController.java
@@ -12,13 +12,11 @@ import com.baio.crawlerapi.service.CrawlerService;
 
 @RestController
 public class CrawlerController {
-    
+
     @GetMapping("/catalog")
     public List<MoviesCatalogDto> getSites(@RequestParam(defaultValue="1", required=false) Integer page){
         CrawlerService crawlerService = new CrawlerService();
-
-        return crawlerService.crawler("https://comando.la/page/"+page);
-
+        return crawlerService.crawler("http://comando.la/page/"+page);
     }
 
     @GetMapping("/magnet")
@@ -26,6 +24,15 @@ public class CrawlerController {
         CrawlerService crawlerService = new CrawlerService();
 
         return crawlerService.getMagnet(url);
+    }
+
+    @GetMapping("/search")
+    public List<MoviesCatalogDto> getAllBySearch(@RequestParam String search){
+        CrawlerService crawlerService = new CrawlerService();
+
+
+
+        return crawlerService.crawler("https://comando.la/?s="+search);
     }
 
 
